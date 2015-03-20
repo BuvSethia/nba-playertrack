3faa13cff678415052fd8ed627b9ebdff59d898a
Buv Sethia <cheeto10x@yahoo.com>
2015-03-20 15:45:09 -0700

    Article tab fully functional, but some UI changes might be needed

---
+++ NBAPlayerTrack/Article.h
@@ -0,0 +1,13 @@
+//
+//  Article.h
+//  NBAPlayerTrack
+//
+//  Created by Buv Sethia on 3/19/15.
+//  Copyright (c) 2015 ___Sethia___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Article : NSObject
+
+@end
---
+++ NBAPlayerTrack/Article.m
@@ -0,0 +1,13 @@
+//
+//  Article.m
+//  NBAPlayerTrack
+//
+//  Created by Buv Sethia on 3/19/15.
+//  Copyright (c) 2015 ___Sethia___. All rights reserved.
+//
+
+#import "Article.h"
+
+@implementation Article
+
+@end
