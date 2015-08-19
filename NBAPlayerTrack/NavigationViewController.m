24fd8e04893d57463e35999360c42428ed275c13
Buv Sethia <cheeto10x@yahoo.com>
2015-08-18 17:38:54 -0700

    Created View Controllers for each menu option and added the segues

--- NBAPlayerTrack/NavigationViewController.m
+++ NBAPlayerTrack/NavigationViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "NavigationViewController.h"
+#import "MainMenuViewController.h"
 
 @implementation NavigationViewController
 
 -(void)viewDidLoad
 {
     self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
-    self.menuItems = [[NSArray alloc] initWithObjects:@"Main Menu", @"Around the NBA", @"Player Compare", @"Options", @"References", nil];
+    self.menuItems = [[NSArray alloc] initWithObjects:@"Main Menu", @"Around the NBA", @"Player Compare", @"Graphs", @"Options", @"References", nil];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -27,6 +28,36 @@
     cell.textLabel.text = self.menuItems[indexPath.row];
     
     return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *selectedOption = self.menuItems[indexPath.row];
+    
+    if([selectedOption isEqualToString:@"Main Menu"])
+    {
+        [self performSegueWithIdentifier:@"MainMenuSegue" sender:self];
+    }
+    else if([selectedOption isEqualToString:@"Around the NBA"])
+    {
+        [self performSegueWithIdentifier:@"AroundTheNBASegue" sender:self];
+    }
+    else if([selectedOption isEqualToString:@"Player Compare"])
+    {
+        [self performSegueWithIdentifier:@"PlayerCompareSegue" sender:self];
+    }
+    else if([selectedOption isEqualToString:@"Graphs"])
+    {
+        [self performSegueWithIdentifier:@"GraphSegue" sender:self];
+    }
+    else if([selectedOption isEqualToString:@"Options"])
+    {
+        [self performSegueWithIdentifier:@"OptionsSegue" sender:self];
+    }
+    else if([selectedOption isEqualToString:@"References"])
+    {
+        [self performSegueWithIdentifier:@"ReferencesSegue" sender:self];
+    }
 }
 
 @end
