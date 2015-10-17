05c0ed7ead3db906d71daf84b95ed35f88d5949b
Buv Sethia <cheeto10x@yahoo.com>
2015-10-16 19:43:00 -0700

    Clicking on a stat will now bring up a description of what that stat is.

--- NBAPlayerTrack/PlayerCompareViewController.m
+++ NBAPlayerTrack/PlayerCompareViewController.m
@@ -11,6 +11,8 @@
 
 @interface PlayerCompareViewController ()
 
+@property NSDictionary *statAcronyms;
+
 @end
 
 @implementation PlayerCompareViewController
@@ -48,6 +50,9 @@
     
     //Get rid of extra horizontal lines from table view
     self.statCompareTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    //Stat acronyms
+    self.statAcronyms = [self createStatsAcronymDictionary];
 }
 
 #pragma mark - Navigation
@@ -242,6 +247,14 @@
     }
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Cell selected");
+    UILabel *statName = (UILabel*) [[tableView cellForRowAtIndexPath:indexPath] viewWithTag:101];
+    UIAlertView *statDesc = [[UIAlertView alloc] initWithTitle:statName.text message:[self.statAcronyms objectForKey:statName.text] delegate:Nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+    [statDesc show];
+}
+
 #pragma mark - PopupMenuViewController
 -(void)selectedMenuItem:(NSInteger)newItem calledBy:(id)sender
 {
@@ -272,6 +285,62 @@
     }
     
     [self.statCompareTableView reloadData];
+}
+
+//What each stat acronym stands for
+-(NSMutableDictionary*)createStatsAcronymDictionary
+{
+    NSMutableDictionary *statsAcronyms = [[NSMutableDictionary alloc] init];
+    [statsAcronyms setValue:@"PTS" forKey:@"Points - The number of points a player scores per game."];
+    [statsAcronyms setValue:@"FGA" forKey:@"Field Goal Attempts - The number of shots a player attempts per game."];
+    [statsAcronyms setValue:@"BLK" forKey:@"Blocks - The number of shots a player blocks per game."];
+    [statsAcronyms setValue:@"MP" forKey:@"Minutes Played - The number of minutes a player plays per game (48 minutes in a game excluding overtime)."];
+    [statsAcronyms setValue:@"3PA" forKey:@"3-Point Attempts - The number of 3-point shots a player attempts per game."];
+    [statsAcronyms setValue:@"3PPCT" forKey:@"3-Point % -  The percent of his 3-point attempts a player makes."];
+    [statsAcronyms setValue:@"STL" forKey:@"Steals - The number of steals a player gets per game"];
+    [statsAcronyms setValue:@"FT" forKey:@"Free Throws - The number of free throws a player makes per game."];
+    [statsAcronyms setValue:@"G" forKey:@"Games - The number of games a player has played."];
+    [statsAcronyms setValue:@"2PA" forKey:@"2-Point Attempts - The number of 2-point shots a player attempts per game."];
+    [statsAcronyms setValue:@"2PPCT" forKey:@"2-Point % - The percent of his 2-point attempts a player makes."];
+    [statsAcronyms setValue:@"FG" forKey:@"Field Goals - The number of shots a player makes per game."];
+    [statsAcronyms setValue:@"FTPCT" forKey:@"Free Throw % - The percent of his free throw attempts a player makes."];
+    [statsAcronyms setValue:@"TRB" forKey:@"Total Rebounds - The number of rebounds a player grabs per game (both offensive and defensive)."];
+    [statsAcronyms setValue:@"FGPCT" forKey:@"Field Goal % - The percent of his shots a player makes."];
+    [statsAcronyms setValue:@"DRB" forKey:@"Defensive Rebounds - The number of defensive rebounds a player grabs per game."];
+    [statsAcronyms setValue:@"TOV" forKey:@"Turnovers - The number of turnovers a player commits per game."];
+    [statsAcronyms setValue:@"ORB" forKey:@"Offensive Rebounds - The number of offensive rebounds a player grabs per game."];
+    [statsAcronyms setValue:@"2P" forKey:@"2-Pointers - The number of 2-point shots a player makes per game."];
+    [statsAcronyms setValue:@"PF" forKey:@"Personal Fouls - The number of fouls a player commits per game."];
+    [statsAcronyms setValue:@"3P" forKey:@"3-Pointers - The number of 3-point shots a player makes per game."];
+    [statsAcronyms setValue:@"Season" forKey:@"The current season."];
+    [statsAcronyms setValue:@"AST" forKey:@"Assists - The number of assists a player gets per game (an assist is received when a player makes a pass to a teammate resulting in a score)."];
+    [statsAcronyms setValue:@"FTA" forKey:@"Free Throw Attempts - The number of free throws a player attempts per game."];
+    [statsAcronyms setValue:@"STLPCT" forKey:@"Steal % - The percentage of opponent possessions that end with a steal by the player while he is on the floor."];
+    [statsAcronyms setValue:@"DRBPCT" forKey:@"Defensive Rebound % - The percentage of available defensive rebounds a player grabs while he is on the floor."];
+    [statsAcronyms setValue:@"WS48" forKey:@"Win Shares per 48 Minutes - The number of win shares a player receives per 48 minutes played (normalizing to 48 minutes attempts to account for the fact that not all players get the same playing time)."];
+    [statsAcronyms setValue:@"3PAr" forKey:@"3-Point Attempt Rate - The percentage of a player's shots that are 3-point shots."];
+    [statsAcronyms setValue:@"TRBPCT" forKey:@"Total Rebound % - The percentage of all available rebounds a player grabs while he is on the floor."];
+    [statsAcronyms setValue:@"ASTPCT" forKey:@"Assist % - The percentage of teammate field goals a player assisted while he is on the floor."];
+    [statsAcronyms setValue:@"DWS" forKey:@"Defensive Win Shares - The number of win shares a player accumulates on the defensive end."];
+    [statsAcronyms setValue:@"PER" forKey:@"Player Efficiency Rating - A measure of per minute production for a player standardized such that the league average is always 15.00"];
+    [statsAcronyms setValue:@"OWS" forKey:@"Offensive Win Shares - The number of win shares a player accumulates on the offensive end."];
+    [statsAcronyms setValue:@"BLKPCT" forKey:@"Block % - The percentage of opponent 2-point attempts a player blocks while he is on the floor"];
+    [statsAcronyms setValue:@"ORBPCT" forKey:@"Offensive Rebound % - The percentage of available offensive rebounds a player grabs while he is on the floor."];
+    [statsAcronyms setValue:@"OBPM" forKey:@"Offensive Box Plus/Minus - How many more points per 100 possessions a player contributes on the offensive end compared to a league-average player, translated to an average team."];
+    [statsAcronyms setValue:@"WS" forKey:@"Win Shares - How many wins a player contributes to his teams."];
+    [statsAcronyms setValue:@"TOVPCT" forKey:@"Turnover % - How many turnovers a player commits per 100 plays."];
+    [statsAcronyms setValue:@"USGPCT" forKey:@"Usage % - The percent of his team's possessions a player uses while a player is on the floor (possession defined as a play ending in a shot attempt, free throws, or a turnover)."];
+    [statsAcronyms setValue:@"DBPM" forKey:@"Defensive Box Plus/Minus - How many more points per 100 possessions a player contributes on the defensive end compared to a league-average player, translated to an average team."];
+    [statsAcronyms setValue:@"TSPCT" forKey:@"True Shooting % - A measure of shooting efficiency that takes into account the value of 3-point shots and free throws."];
+    [statsAcronyms setValue:@"FTr" forKey:@"Free Throw Rate - The number of free throws a player attempts per field goal attempt."];
+    
+    //Because I'm an idiot and too lazy to switch all keys and values manually
+    for (NSString *key in [statsAcronyms allKeys]) {
+        statsAcronyms[statsAcronyms[key]] = key;
+        [statsAcronyms removeObjectForKey:key];
+    }
+    
+    return statsAcronyms;
 }
 
 @end
